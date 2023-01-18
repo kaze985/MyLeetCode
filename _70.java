75ea9ead185b12974c02e9b69b717214bf14a9e6
kaze8888 <2082856592@qq.com>
2023-01-19 00:21:53 +0800

    add

---
+++ _70.java
@@ -0,0 +1,14 @@
+public class _70 {
+    public int climbStairs(int n) {
+        if (n <= 1) return n;
+        int[] dp = new int[3];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            int sum = dp[1] + dp[2];
+            dp[1] = dp[2];
+            dp[2] = sum;
+        }
+        return dp[2];
+    }
+}
