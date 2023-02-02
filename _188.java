4da3826ee48a1b5c6be4ac843ec85058ed3eea94
kaze8888 <2082856592@qq.com>
2023-02-02 20:31:16 +0800

    add

---
+++ _188.java
@@ -0,0 +1,16 @@
+public class _188 {
+    public int maxProfit(int k, int[] prices) {
+        if (k == 0 || prices.length == 0) return 0;
+        int[][] dp = new int[prices.length][2 * k + 1];
+        for (int j = 1; j < 2 * k; j += 2) {
+            dp[0][j] = -prices[0];
+        }
+        for (int i = 1; i < prices.length; i++) {
+            for (int j = 0; j < 2 * k - 1; j += 2) {
+                dp[i][j + 1] = Math.max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i]);
+                dp[i][j + 2] = Math.max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i]);
+            }
+        }
+        return dp[prices.length - 1][2 * k];
+    }
+}
--- _714.java
+++ _714.java
@@ -1,19 +1,30 @@
 public class _714 {
     public int maxProfit(int[] prices, int fee) {
-        int result = 0;
-        int minPrice = prices[0];
+//        贪心解法
+//        int result = 0;
+//        int minPrice = prices[0];
+//        for (int i = 1; i < prices.length; i++) {
+//            if (prices[i] < minPrice) {
+//                minPrice = prices[i];
+//            }
+//            if (prices[i] >= minPrice && prices[i] <= minPrice + fee) {
+//                continue;
+//            }
+//            if (prices[i] > minPrice + fee) {
+//                result += prices[i] - minPrice - fee;
+//                minPrice = prices[i] - fee;
+//            }
+//        }
+//        return result;
+
+//        动态规划
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] -= prices[0];
+        dp[0][1] = 0;
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            }
-            if (prices[i] >= minPrice && prices[i] <= minPrice + fee) {
-                continue;
-            }
-            if (prices[i] > minPrice + fee) {
-                result += prices[i] - minPrice - fee;
-                minPrice = prices[i] - fee;
-            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], prices[i] + dp[i - 1][0] - fee);
         }
-        return result;
+        return dp[prices.length - 1][1];
     }
 }
