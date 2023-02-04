861e4113202a433aa0bd48d2c20b0036fb83754d
kaze8888 <2082856592@qq.com>
2023-02-04 22:17:15 +0800

    add

--- _53.java
+++ _53.java
@@ -1,15 +1,26 @@
 public class _53 {
     public int maxSubArray(int[] nums) {
-        int result = Integer.MIN_VALUE;
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            count += nums[i];
-            if (count > result) {
-                result = count;
-            }
-            if (count <= 0) {
-                count = 0;
-            }
+//        贪心解法
+//        int result = Integer.MIN_VALUE;
+//        int count = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            count += nums[i];
+//            if (count > result) {
+//                result = count;
+//            }
+//            if (count <= 0) {
+//                count = 0;
+//            }
+//        }
+//        return result;
+
+//        动态规划
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int result = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+            if (dp[i] > result) result = dp[i];
         }
         return result;
     }
