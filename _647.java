0115bb744da1e4970fc13ca01dcb1f3ac602a512
kaze8888 <2082856592@qq.com>
2023-02-19 11:48:05 +0800

    add

---
+++ _647.java
@@ -0,0 +1,21 @@
+public class _647 {
+    public int countSubstrings(String s) {
+        int length = s.length();
+        int result = 0;
+        boolean[][] dp = new boolean[length][length];
+        for (int i = length - 1; i >= 0; i--) {
+            for (int j = i; j < length; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i <= 1) {
+                        result++;
+                        dp[i][j] = true;
+                    } else if (dp[i + 1][j - 1]) {
+                        result++;
+                        dp[i][j] = true;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
